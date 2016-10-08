82f688d6cb126aef5285ec1cf312a07c68b14324
yuanyf <g.yuanyf@gmail.com>
2016-10-08 15:49:54 +0800

    修复缓存问题

--- src/main/java/org/cboard/services/CachedDataProviderService.java
+++ src/main/java/org/cboard/services/CachedDataProviderService.java
@@ -3,6 +3,8 @@ package org.cboard.services;
 import org.cboard.dto.DataProviderResult;
 import org.springframework.stereotype.Repository;
 
+import javax.servlet.ServletRequestEvent;
+import javax.servlet.ServletRequestListener;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,17 +12,13 @@ import java.util.Map;
  * Created by yfyuan on 2016/8/24.
  */
 @Repository
-public class CachedDataProviderService extends DataProviderService {
+public class CachedDataProviderService extends DataProviderService implements ServletRequestListener {
 
     private static ThreadLocal<Map<String, DataProviderResult>> cache = new ThreadLocal<>();
 
     @Override
     public DataProviderResult getData(Long datasourceId, Map<String, String> query) {
         Map<String, DataProviderResult> map = cache.get();
-        if (map == null) {
-            cache.set(new HashMap<String, DataProviderResult>());
-            map = cache.get();
-        }
 
         String keys = "" + datasourceId + "_" + query.toString();
 
@@ -30,5 +28,15 @@ public class CachedDataProviderService extends DataProviderService {
             map.put(keys, data);
         }
         return data;
+    }
+
+    @Override
+    public void requestDestroyed(ServletRequestEvent servletRequestEvent) {
+        cache.remove();
+    }
+
+    @Override
+    public void requestInitialized(ServletRequestEvent servletRequestEvent) {
+        cache.set(new HashMap<String, DataProviderResult>());
     }
 }

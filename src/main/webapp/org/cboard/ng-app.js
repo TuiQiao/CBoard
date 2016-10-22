11dd6acc7fd34c7ad0a29edcb9464b1496f57f32
yzhang921 <peter.zhang921@gmail.com>
2016-10-22 20:32:03 +0800

    ready for lazy load and internationalization
    move angular module intial script and configuration file to ng-app.js and ng-config.js

---
+++ src/main/webapp/org/cboard/ng-app.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Peter on 2016/10/22.
+ */
+
+var cBoard = angular.module('cBoard', ['ui.router', 'angular-md5', 'dndLists',
+    'ui.bootstrap','ngSanitize','ui.select']);
---
+++ src/main/webapp/org/cboard/ng-config.js
@@ -0,0 +1,133 @@
+/**
+ * Created by Peter on 2016/10/22.
+ */
+
+
+angular.module('cBoard').config(['$stateProvider', function ($stateProvider) {
+    $stateProvider
+        .state('dashboard', {
+            url: '/dashboard',
+            abstract: true,
+            template: '<div ui-view></div>'
+        })
+        .state('mine', {
+            url: '/mine',
+            abstract: true,
+            template: '<div ui-view></div>'
+        })
+        .state('mine.view', {
+            url: '/{id}',
+            params: {id: null},
+            templateUrl: 'org/cboard/view/dashboard/view.html',
+            controller: 'dashboardViewCtrl'
+        })
+        .state('dashboard.category', {
+            url: '/{category}',
+            params: {category: null},
+            abstract: true,
+            template: '<div ui-view></div>',
+        })
+        .state('dashboard.category.view', {
+            url: '/{id}',
+            params: {id: null},
+            templateUrl: 'org/cboard/view/dashboard/view.html',
+            controller: 'dashboardViewCtrl'
+        })
+        .state('config', {
+            url: '/config',
+            abstract: true,
+            template: '<div ui-view></div>'
+        })
+        .state('config.board', {
+            url: '/board',
+            templateUrl: 'org/cboard/view/config/board.html',
+            controller: 'boardCtrl'
+        })
+        .state('config.widget', {
+            url: '/widget/{id}',
+            params: {id: null},
+            templateUrl: 'org/cboard/view/config/widget.html',
+            controller: 'widgetCtrl'
+        })
+        .state('config.datasource', {
+            url: '/datasource',
+            templateUrl: 'org/cboard/view/config/datasource.html',
+            controller: 'datasourceCtrl'
+        })
+        .state('config.category', {
+            url: '/category',
+            templateUrl: 'org/cboard/view/config/category.html',
+            controller: 'categoryCtrl'
+        }).state('config.dataset', {
+        url: '/dataset',
+        templateUrl: 'org/cboard/view/config/dataset.html',
+        controller: 'datasetCtrl'
+    });
+
+}]);
+
+angular.module('cBoard').factory('sessionHelper', ["$rootScope", function ($rootScope) {
+    var sessionHelper = {
+        responseError: function (response) {
+            if (response.status == -1) {
+                window.location.href = "/";
+            } else {
+                return response;
+            }
+        }
+    };
+    return sessionHelper;
+}]);
+
+
+angular.module('cBoard').config(function ($httpProvider) {
+    $httpProvider.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded';
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+
+    // Override $http service's default transformRequest
+    $httpProvider.defaults.transformRequest = [function (data) {
+        /**
+         * The workhorse; converts an object to x-www-form-urlencoded serialization.
+         * @param {Object} obj
+         * @return {String}
+         */
+        var param = function (obj) {
+            var query = '';
+            var name, value, fullSubName, subName, subValue, innerObj, i;
+
+            for (name in obj) {
+                value = obj[name];
+
+                if (value instanceof Array) {
+                    for (i = 0; i < value.length; ++i) {
+                        subValue = value[i];
+                        fullSubName = name + '[' + i + ']';
+                        innerObj = {};
+                        innerObj[fullSubName] = subValue;
+                        query += param(innerObj) + '&';
+                    }
+                } else if (value instanceof Object) {
+                    for (subName in value) {
+                        subValue = value[subName];
+                        fullSubName = name + '[' + subName + ']';
+                        innerObj = {};
+                        innerObj[fullSubName] = subValue;
+                        query += param(innerObj) + '&';
+                    }
+                } else if (value !== undefined && value !== null) {
+                    query += encodeURIComponent(name) + '='
+                        + encodeURIComponent(value) + '&';
+                }
+            }
+
+            return query.length ? query.substr(0, query.length - 1) : query;
+        };
+
+        return angular.isObject(data) && String(data) !== '[object File]'
+            ? param(data)
+            : data;
+    }];
+
+    $httpProvider.interceptors.push('sessionHelper');
+
+});

a478ea4e531d3e5030e196727852151ea259195e
yfyuan <pentiumsk@gmail.com>
2016-12-01 10:45:29 +0800

    fix table height in realtime

--- src/main/webapp/org/cboard/util/CBoardTableRender.js
+++ src/main/webapp/org/cboard/util/CBoardTableRender.js
@@ -49,7 +49,7 @@ CBoardTableRender.prototype.do = function (tall) {
     tall = _.isUndefined(tall) ? 600 : tall;
     var divHeight = tall - 40;
     var exportBnt = "<div class='exportBnt'><button>export</button></div>";
-    $(this.container).append(exportBnt);
+    $(this.container).html(exportBnt);
     $(this.container).append("<div style='width: 100%;height:" + divHeight + "px;overflow: auto'>" + html + "</div>");
     $(this.container).css({
         height: tall + 40 + "px"

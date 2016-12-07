db32ee8951511a27763f26ab74ff283c71b467fe
yfyuan <pentiumsk@gmail.com>
2016-12-07 18:02:55 +0800

    #53 sql

--- sql/mysql/mysql.sql
+++ sql/mysql/mysql.sql
@@ -50,6 +50,8 @@ CREATE TABLE `dashboard_user` (
   PRIMARY KEY (`user_id`)
 );
 
+INSERT INTO `dashboard_user` (`user_id`,`login_name`,`user_name`,`user_password`) VALUES('1','admin','管理员','ff9830c42660c1dd1942844f8069b74a');
+
 CREATE TABLE `dashboard_user_role` (
   `user_role_id` bigint(20) NOT NULL AUTO_INCREMENT,
   `user_id` varchar(100) DEFAULT NULL,

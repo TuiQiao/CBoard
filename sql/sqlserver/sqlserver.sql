76713fe24f9712ce0b8713f9e4789856886588ad
yzhang921 <peter.zhang921@gmail.com>
2016-12-21 11:02:40 +0800

    update sql script for meta data

--- sql/sqlserver/sqlserver.sql
+++ sql/sqlserver/sqlserver.sql
@@ -1,5 +1,5 @@
 --
-CREATE DATABASE AdminBoard;
+CREATE DATABASE CBoard;
 
 CREATE TABLE dashboard_board (
   board_id bigint identity(1,1),
@@ -42,4 +42,37 @@ CREATE TABLE dashboard_dataset (
   dataset_name varchar(100) DEFAULT NULL,
   data_json text,
   PRIMARY KEY CLUSTERED(dataset_id)
+);
+
+CREATE TABLE dashboard_user (
+  user_id varchar(50) NOT NULL,
+  login_name varchar(100) DEFAULT NULL,
+  user_name varchar(100) DEFAULT NULL,
+  user_password varchar(100) DEFAULT NULL,
+  user_status varchar(100) DEFAULT NULL,
+  PRIMARY KEY CLUSTERED(user_id)
+);
+
+INSERT INTO dashboard_user (user_id,login_name,user_name,user_password)
+VALUES('1','admin','Administrator','ff9830c42660c1dd1942844f8069b74a');
+
+CREATE TABLE dashboard_user_role (
+  user_role_id bigint identity(1,1),
+  user_id varchar(100) DEFAULT NULL,
+  role_id varchar(100) DEFAULT NULL,
+  PRIMARY KEY CLUSTERED(user_role_id)
+);
+
+CREATE TABLE dashboard_role (
+  role_id varchar(100) NOT NULL,
+  role_name varchar(100) DEFAULT NULL,
+  PRIMARY KEY CLUSTERED(role_id)
+);
+
+CREATE TABLE dashboard_role_res (
+  role_res_id bigint identity(1,1),
+  role_id varchar(100) DEFAULT NULL,
+  res_type varchar(100) DEFAULT NULL,
+  res_id bigint DEFAULT NULL,
+  PRIMARY KEY CLUSTERED(role_res_id)
 );

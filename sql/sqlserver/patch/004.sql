8973561daf834b6149ed655e156dfa7702141ee5
yfyuan <pentiumsk@gmail.com>
2017-02-23 15:56:18 +0800

    mail send #74 #75 sql script

---
+++ sql/sqlserver/patch/004.sql
@@ -0,0 +1,12 @@
+CREATE TABLE dashboard_job (
+  job_id bigint identity(1,1),
+  job_name varchar(200) DEFAULT NULL,
+  cron_exp varchar(200) DEFAULT NULL,
+  start_date datetime NULL DEFAULT NULL,
+  end_date datetime NULL DEFAULT NULL,
+  job_type varchar(200) DEFAULT NULL,
+  job_config text,
+  user_id varchar(100) DEFAULT NULL,
+  last_exec_time datetime NULL DEFAULT NULL,
+  PRIMARY KEY CLUSTERED (job_id)
+);

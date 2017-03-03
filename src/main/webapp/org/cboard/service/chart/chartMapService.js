abf3e961c0d01dc97aea8e22be792f4d36e9933d
yzhang921 <peter.zhang921@gmail.com>
2017-03-03 18:26:36 +0800

    Fix bug when there is no data, exception raise when table and map render

--- src/main/webapp/org/cboard/service/chart/chartMapService.js
+++ src/main/webapp/org/cboard/service/chart/chartMapService.js
@@ -3,6 +3,10 @@
  */
 cBoard.service('chartMapService', function (dataService) {
     this.render = function (containerDom, option, scope, persist) {
+        if (option == null) {
+            containerDom.html("<div class=\"alert alert-danger\" role=\"alert\">No Data!</div>");
+            return;
+        }
         var height;
         scope ? height = scope.myheight - 20 : null;
         return new CBoardMapRender(containerDom, option).do(height, persist);
@@ -10,6 +14,9 @@ cBoard.service('chartMapService', function (dataService) {
 
     this.parseOption = function (chartData, chartConfig) {
         var mapOption = null;
+        if (chartData.data.length == 0) {
+            return null;
+        }
         dataService.castRawData2Series(chartData, chartConfig, function (casted_keys, casted_values, aggregate_data, newValuesConfig) {
             mapOption = chartDataProcess(chartConfig,casted_keys, casted_values, aggregate_data,newValuesConfig);
         });

e8e55d377d7256ebef1f1179a0fad856ace7459d
hjtoday <972546183@qq.com>
2017-07-11 15:22:24 +0800

    fileprovider add csv

--- src/main/java/org/cboard/filedp/FileDataProvider.java
+++ src/main/java/org/cboard/filedp/FileDataProvider.java
@@ -1,5 +1,6 @@
 package org.cboard.filedp;
 
+import au.com.bytecode.opencsv.CSVReader;
 import com.alibaba.fastjson.JSON;
 import org.apache.commons.lang.StringUtils;
 import org.cboard.dataprovider.DataProvider;
@@ -33,20 +34,20 @@ public class FileDataProvider extends DataProvider {
     @QueryParameter(label = "{{'DATAPROVIDER.TEXTFILE.FILE_NAME'|translate}}", type = QueryParameter.Type.Input, order = 1)
     private String QUERY_PARAM_FILE_NAME = "fileName";
 
-    @QueryParameter(label = "{{'DATAPROVIDER.TEXTFILE.DATA_TYPE'|translate}}", options = {"DSV", "JSON", "CSV"}, type = QueryParameter.Type.Select, order = 2)
-    private String QUERY_PARAM_DATA_TYPE = "dataType";
-
-    @QueryParameter(label = "{{'DATAPROVIDER.TEXTFILE.FIELD_NAMES'|translate}}", placeholder = "(Please don't input fields exist in file)<fieldName>[,<fieldName>]...", type = QueryParameter.Type.Input, order = 3)
-    private String QUERY_PARAM_FIELD_NAMES = "fieldName";
-
-    @QueryParameter(label = "{{'DATAPROVIDER.TEXTFILE.ENCODING'|translate}}", value="UTF-8", placeholder = "default value is UTF-8", type = QueryParameter.Type.Input, order = 4)
+    @QueryParameter(label = "{{'DATAPROVIDER.TEXTFILE.ENCODING'|translate}}", value="UTF-8", placeholder = "default value is UTF-8", type = QueryParameter.Type.Input, order = 2)
     private String QUERY_PARAM_ENCODING = "encoding";
 
-    @QueryParameter(label = "{{'DATAPROVIDER.TEXTFILE.SEPRATOR'|translate}}", value="\\\\t", placeholder = "default value is \\t", type = QueryParameter.Type.Input, order = 5)
+    @QueryParameter(label = "{{'DATAPROVIDER.TEXTFILE.DATA_TYPE'|translate}}", options = {"DSV", "JSON", "CSV"}, type = QueryParameter.Type.Select, order = 3)
+    private String QUERY_PARAM_DATA_TYPE = "dataType";
+
+    @QueryParameter(label = "{{'DATAPROVIDER.TEXTFILE.FIELD_NAMES'|translate}}", placeholder = "<fieldName>[,<fieldName>]...(The file has a field header without input)", type = QueryParameter.Type.Input, order = 4)
+    private String QUERY_PARAM_FIELD_NAMES = "fieldName";
+
+    @QueryParameter(label = "{{'DATAPROVIDER.TEXTFILE.SEPRATOR'|translate}}", value="\\\\t", placeholder = "default value is \\t (The DataType is JSON without input)", type = QueryParameter.Type.Input, order = 5)
     private String QUERY_PARAM_SEPRATOR = "seprator";
 
-    @QueryParameter(label = "{{'DATAPROVIDER.TEXTFILE.ENCLOSURE'|translate}}", type = QueryParameter.Type.Input, order = 6)
-    private String QUERY_PARAM_ENCLOSURE = "enclosure";
+    @QueryParameter(label = "{{'DATAPROVIDER.TEXTFILE.QUOTECHAR'|translate}}", type = QueryParameter.Type.Input, order = 6)
+    private String QUERY_PARAM_QUOTECHAR = "quoteChar";
 
     @Override
     public boolean doAggregationInDataSource() {
@@ -59,8 +60,6 @@ public class FileDataProvider extends DataProvider {
         String fileName = query.get(QUERY_PARAM_FILE_NAME);
         String encoding = query.get(QUERY_PARAM_ENCODING);
         String seprator = query.get(QUERY_PARAM_SEPRATOR);
-        String enclosure = query.getOrDefault(QUERY_PARAM_ENCLOSURE, "");
-        String filedNames = query.get(QUERY_PARAM_FIELD_NAMES);
         String dataType =  query.get(QUERY_PARAM_DATA_TYPE);
 
         encoding = StringUtils.isBlank(encoding) ? "UTF-8" : encoding;
@@ -68,110 +67,157 @@ public class FileDataProvider extends DataProvider {
 
         String fullPath = basePath + fileName;
         LOG.info("INFO: Read file from {}", fullPath);
-        File file = new File(fullPath);
+        BufferedReader reader = getBufferedReader(fullPath, encoding);
 
-        List<String[]> result = null;
         String[][] strings = null;
-        try (
-                FileInputStream fis = new FileInputStream(file);
-                InputStreamReader isr = new InputStreamReader(fis, encoding);
-                BufferedReader reader = new BufferedReader(isr);
-        ) {
-            if("JSON".equalsIgnoreCase(dataType)){
-                String tempString = null;
-                List<Map> mapList = new ArrayList<Map>();
-                try {
-                    int line = 0;
-                    List<Map> list = new ArrayList<Map>();
-                    while ((tempString = reader.readLine()) != null) {
-                        if (StringUtils.isBlank(tempString.trim())) {
-                            continue;
-                        }
-                        if (line++ > resultLimit) {
-                            throw new CBoardException("Cube result count is greater than limit " + resultLimit);
-                        }
-                        Map map = JSON.parseObject(tempString, Map.class);
-                        mapList.add(map);
-                    }
-                }catch (Exception ex){
-                    StringBuffer sb = new StringBuffer();
-                    while (tempString != null) {
-                        sb.append(tempString);
-                        tempString = reader.readLine();
-                    }
-                    mapList = JSON.parseArray(sb.toString(), Map.class);
-                    if (mapList.size() > resultLimit) {
-                        throw new CBoardException("Cube result count is greater than limit " + resultLimit);
-                    }
-                }
-                if(mapList!=null && mapList.size()>0){
-                    Set<Map.Entry<String, Object>> entrySet = mapList.get(0).entrySet();
-                    strings = new String[mapList.size()+1][entrySet.size()];
-                    int col=0;
-                    for(Map.Entry<String, Object> entry:entrySet){
-                        strings[0][col] = entry.getKey();
-                        col++;
-                    }
-
-                    for (int i = 1; i <= mapList.size(); i++) {
-                        int j = 0;
-                        for (Map.Entry<String, Object> e : entrySet) {
-                            strings[i][j] = String.valueOf(mapList.get(i-1).get(e.getKey()));
-                            j++;
-                        }
-                    }
-                }
-            }else if("DSV".equalsIgnoreCase(dataType)){
-                String tempString = null;
-                result = new LinkedList<>();
-                int line = 0;
-                //如果有filedNames则加到第一行
-                if (StringUtils.isNotEmpty(filedNames)) {
-                    result.add(filedNames.split(","));
-                }
-                // Read line by line
-                while ((tempString = reader.readLine()) != null) {
-                    if (StringUtils.isBlank(tempString.trim())) {
-                        continue;
-                    }
-
-                    if (line++ > resultLimit) {
-                        throw new CBoardException("Cube result count is greater than limit " + resultLimit);
-                    }
-
-                    List<String> lineList = Arrays.asList(tempString.split(seprator)).stream().map(column -> {
-                        return column.replaceAll(enclosure, "");
-                    }).collect(toList());
-                    result.add(lineList.toArray(new String[lineList.size()]));
-                }
-                strings = result.toArray(new String[][]{});
-
-            }else if("CSV".equalsIgnoreCase(dataType)){
-                // TODO... CSV
-            }
-
-            reader.close();
-        } catch (Exception e) {
-            LOG.error("ERROR:" + e.getMessage());
-            throw new Exception("ERROR:" + e.getMessage(), e);
+        if("JSON".equalsIgnoreCase(dataType)){
+            strings = dealJSON(reader);
+        }else if("DSV".equalsIgnoreCase(dataType)){
+            strings = dealDSV(reader, seprator);
+        }else if("CSV".equalsIgnoreCase(dataType)){
+            strings = dealCSV(reader, seprator);
         }
+        reader.close();
         return strings;
     }
 
-
-    public static void main(String[] args) {
+    public BufferedReader getBufferedReader(String filePath, String encoding) throws Exception {
+        BufferedReader reader = null;
         try {
-            BufferedReader br=new BufferedReader(new FileReader("G:\\json.txt"));
-            String line = br.readLine();
-            System.out.println(line);
-
-            br.reset();
-            line = br.readLine();
-            System.out.print(line);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            FileInputStream fis = new FileInputStream(filePath);
+            InputStreamReader isr = new InputStreamReader(fis, encoding);
+            reader = new BufferedReader(isr);
+        }catch (Exception e) {
+            LOG.error("ERROR:" + e.getMessage());
+            throw new Exception("ERROR:" + e.getMessage(), e);
         }
+        return reader;
+    }
 
+    private CSVReader newReader(Reader reader, char separator, char quote, char escape) {
+        if ('"' == escape) {
+            return new CSVReader(reader, separator, quote);
+        }
+        return new CSVReader(reader, separator, quote, escape);
+    }
+
+    public String[][] dealCSV(BufferedReader reader, String seprators){
+        if(query.getOrDefault(QUERY_PARAM_QUOTECHAR, "").toCharArray().length>1 || seprators.toCharArray().length>1){
+            throw new CBoardException("quoteChar and seprator must be char when DataType is CSV");
+        }
+        char quoteChar = query.getOrDefault(QUERY_PARAM_QUOTECHAR, "").charAt(0);
+        char seprator = seprators.charAt(0);
+        String filedNames = query.get(QUERY_PARAM_FIELD_NAMES);
+        CSVReader csv = null;
+        String tempString = null;
+        List<String[]> result = new LinkedList<>();
+        int line = 0;
+        //如果有filedNames则加到第一行
+        if (StringUtils.isNotEmpty(filedNames)) {
+            result.add(filedNames.split(","));
+        }
+        try {
+            // Read line by line
+            while ((tempString = reader.readLine()) != null) {
+                if (StringUtils.isBlank(tempString.trim())) {
+                    continue;
+                }
+                if (line++ > resultLimit) {
+                    throw new CBoardException("Cube result count is greater than limit " + resultLimit);
+                }
+                csv = newReader(new CharArrayReader(tempString.toCharArray()),seprator, quoteChar, '\\');
+                result.add(csv.readNext());
+            }
+        } catch (Exception e) {
+            LOG.error("ERROR:" + e.getMessage());
+            throw new CBoardException("ERROR:" + e.getMessage());
+        }
+        return result.toArray(new String[][]{});
+    }
+
+    public String[][] dealDSV(BufferedReader reader, String seprator){
+        String quoteChar = query.getOrDefault(QUERY_PARAM_QUOTECHAR, "");
+        String filedNames = query.get(QUERY_PARAM_FIELD_NAMES);
+        String tempString = null;
+        List<String[]> result = new LinkedList<>();
+        int line = 0;
+        //如果有filedNames则加到第一行
+        if (StringUtils.isNotEmpty(filedNames)) {
+            result.add(filedNames.split(","));
+        }
+        try {
+            // Read line by line
+            while ((tempString = reader.readLine()) != null) {
+                if (StringUtils.isBlank(tempString.trim())) {
+                    continue;
+                }
+
+                if (line++ > resultLimit) {
+                    throw new CBoardException("Cube result count is greater than limit " + resultLimit);
+                }
+                List<String> lineList = Arrays.asList(tempString.split(seprator)).stream().map(column -> {
+                    return column.replaceAll(quoteChar, "");
+                }).collect(toList());
+                result.add(lineList.toArray(new String[lineList.size()]));
+            }
+        } catch (IOException e) {
+            LOG.error("ERROR:" + e.getMessage());
+            throw new CBoardException("ERROR:" + e.getMessage());
+        }
+        return result.toArray(new String[][]{});
+    }
+
+    public String[][] dealJSON(BufferedReader reader){
+        String[][] strings = null;
+        String tempString = null;
+        List<Map> mapList = new ArrayList<Map>();
+        try {
+            int line = 0;
+            List<Map> list = new ArrayList<Map>();
+            while ((tempString = reader.readLine()) != null) {
+                if (StringUtils.isBlank(tempString.trim())) {
+                    continue;
+                }
+                if (line++ > resultLimit) {
+                    throw new CBoardException("Cube result count is greater than limit " + resultLimit);
+                }
+                Map map = JSON.parseObject(tempString, Map.class);
+                mapList.add(map);
+            }
+        }catch (Exception ex){
+            StringBuffer sb = new StringBuffer();
+            while (tempString != null) {
+                sb.append(tempString);
+                try {
+                    tempString = reader.readLine();
+                } catch (IOException e) {
+                    LOG.error("ERROR:" + e.getMessage());
+                    throw new CBoardException("ERROR:" + e.getMessage());
+                }
+            }
+            mapList = JSON.parseArray(sb.toString(), Map.class);
+            if (mapList.size() > resultLimit) {
+                throw new CBoardException("Cube result count is greater than limit " + resultLimit);
+            }
+        }finally {
+            if(mapList!=null && mapList.size()>0){
+                Set<Map.Entry<String, Object>> entrySet = mapList.get(0).entrySet();
+                strings = new String[mapList.size()+1][entrySet.size()];
+                int col=0;
+                for(Map.Entry<String, Object> entry:entrySet){
+                    strings[0][col] = entry.getKey();
+                    col++;
+                }
+
+                for (int i = 1; i <= mapList.size(); i++) {
+                    int j = 0;
+                    for (Map.Entry<String, Object> e : entrySet) {
+                        strings[i][j] = String.valueOf(mapList.get(i-1).get(e.getKey()));
+                        j++;
+                    }
+                }
+            }
+            return strings;
+        }
     }
 }

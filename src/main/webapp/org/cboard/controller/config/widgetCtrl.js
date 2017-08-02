0b2feb8c253efb2102b76754b62db3d128b63398
Junjie.M <118170653@qq.com>
2017-08-02 16:45:19 +0800

    Update widgetCtrl.js

--- src/main/webapp/org/cboard/controller/config/widgetCtrl.js
+++ src/main/webapp/org/cboard/controller/config/widgetCtrl.js
@@ -97,7 +97,8 @@ cBoard.controller('widgetCtrl', function ($scope, $stateParams, $http, $uibModal
                 row: translate('CONFIG.WIDGET.TIPS_DIM_NUM_1_MORE'),
                 column: translate('CONFIG.WIDGET.TIPS_DIM_NUM_1_MORE'),
                 measure: translate('CONFIG.WIDGET.TIPS_DIM_NUM_1')
-            },{
+            },
+            {
                 name: translate('CONFIG.WIDGET.MARK_LINE_MAP'), value: 'markLineMap', class: 'cMarkLineMap',
                 row: translate('CONFIG.WIDGET.TIPS_DIM_NUM_1_MORE'),
                 column: translate('CONFIG.WIDGET.TIPS_DIM_NUM_0_MORE'),

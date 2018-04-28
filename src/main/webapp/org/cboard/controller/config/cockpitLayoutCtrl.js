29b33259130dc4ec92b1e565dbb9c5939f116f29
peter <peter.zhang921@gmail.com>
2018-04-28 16:38:20 +0800

    Merge 样式修改 #502

--- src/main/webapp/org/cboard/controller/config/cockpitLayoutCtrl.js
+++ src/main/webapp/org/cboard/controller/config/cockpitLayoutCtrl.js
@@ -151,10 +151,10 @@ cBoard.controller('cockpitLayoutCtrl', function ($rootScope, $scope, $stateParam
                     //表格组件默认值
                     cockpitChartData.jsonData.value = hexCockpit.defaultTableData();
                     cockpitChartData.cockpitConfChartCSS = hexCockpit.defaultTableStyle();
-                } else if(dom.type == 'chart') {
-                    if(dom.border){
+                } else if (dom.type == 'chart') {
+                    if (dom.border) {
                         cockpitChartData.border = dom.border;
-                    }else{
+                    } else {
                         cockpitChartData.border = vm._data.borderStyle[0].value;
                     }
 

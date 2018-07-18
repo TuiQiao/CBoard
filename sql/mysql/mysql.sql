a379c7aad54aa187d84074afba866f38bdd07ffe
peter <peter.zhang921@gmail.com>
2018-07-18 15:04:32 +0800

    fix bug Metadata script is not compatible with MySQL version before v5.6.5 #486
    (cherry picked from commit a8581c3)

--- sql/mysql/mysql.sql
+++ sql/mysql/mysql.sql
@@ -1,3 +1,7 @@
+DROP DATABASE IF EXISTS cboard;
+CREATE DATABASE cboard CHARACTER SET utf8;
+USE cboard;
+
 CREATE TABLE dashboard_board (
   board_id bigint(20) NOT NULL AUTO_INCREMENT,
   user_id varchar(50) NOT NULL,
@@ -100,14 +104,38 @@ CREATE TABLE dashboard_board_param (
 );
 
 -- 升级0.4需要执行的
-ALTER  TABLE  dashboard_dataset ADD create_time TIMESTAMP DEFAULT now();
-ALTER  TABLE  dashboard_dataset ADD update_time TIMESTAMP DEFAULT now();
+ALTER TABLE dashboard_dataset ADD create_time TIMESTAMP DEFAULT now();
+ALTER TABLE dashboard_dataset ADD update_time TIMESTAMP;
+UPDATE dashboard_dataset SET update_time = create_time;
+-- Use trigger set update time
+CREATE TRIGGER insert_dataset_update_time_trigger
+BEFORE INSERT ON dashboard_dataset FOR EACH ROW SET new.update_time = now();
+CREATE TRIGGER update_dataset_update_time_trigger
+BEFORE UPDATE ON dashboard_dataset FOR EACH ROW SET new.update_time = now();
 
-ALTER  TABLE  dashboard_datasource ADD create_time TIMESTAMP DEFAULT now();
-ALTER  TABLE  dashboard_datasource ADD update_time TIMESTAMP DEFAULT now();
+ALTER TABLE dashboard_datasource ADD create_time TIMESTAMP DEFAULT now();
+ALTER TABLE dashboard_datasource ADD update_time TIMESTAMP;
+UPDATE dashboard_datasource SET update_time = create_time;
+-- Use trigger set update time
+CREATE TRIGGER insert_datasource_update_time_trigger
+BEFORE INSERT ON dashboard_datasource FOR EACH ROW SET new.update_time = now();
+CREATE TRIGGER update_datasource_update_time_trigger
+BEFORE UPDATE ON dashboard_datasource FOR EACH ROW SET new.update_time = now();
 
-ALTER  TABLE  dashboard_widget ADD create_time TIMESTAMP DEFAULT now();
-ALTER  TABLE  dashboard_widget ADD update_time TIMESTAMP DEFAULT now();
+ALTER TABLE dashboard_widget ADD create_time TIMESTAMP DEFAULT now();
+ALTER TABLE dashboard_widget ADD update_time TIMESTAMP;
+UPDATE dashboard_widget SET update_time = create_time;
+-- Use trigger set update time
+CREATE TRIGGER insert_widget_update_time_trigger
+BEFORE INSERT ON dashboard_widget FOR EACH ROW SET new.update_time = now();
+CREATE TRIGGER update_widget_update_time_trigger
+BEFORE UPDATE ON dashboard_widget FOR EACH ROW SET new.update_time = now();
 
-ALTER  TABLE  dashboard_board ADD create_time TIMESTAMP DEFAULT now();
-ALTER  TABLE  dashboard_board ADD update_time TIMESTAMP DEFAULT now();
+ALTER TABLE dashboard_board ADD create_time TIMESTAMP DEFAULT now();
+ALTER TABLE dashboard_board ADD update_time TIMESTAMP;
+UPDATE dashboard_board SET update_time = create_time;
+-- Use trigger set update time
+CREATE TRIGGER insert_board_update_time_trigger
+BEFORE INSERT ON dashboard_board FOR EACH ROW SET new.update_time = now();
+CREATE TRIGGER update_board_update_time_trigger
+BEFORE UPDATE ON dashboard_board FOR EACH ROW SET new.update_time = now();

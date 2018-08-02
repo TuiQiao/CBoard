5919dcd8899ae34cb23670345df431e20ab6e392
wangkun <kunwang1@jieyuechina.com>
2018-08-02 17:42:53 +0800

    javaconfig 移除lombok

--- src/main/java/org/cboard/config/PropertiesConfig.java
+++ src/main/java/org/cboard/config/PropertiesConfig.java
@@ -1,6 +1,5 @@
 package org.cboard.config;
 
-import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 
@@ -9,7 +8,6 @@ import org.springframework.context.annotation.PropertySource;
  * @create 2018-07-31
  * @desc
  **/
-@Data
 @PropertySource(value = {"classpath:config.properties", "classpath:caso.properties"}, encoding = "UTF-8")
 public class PropertiesConfig {
 
@@ -54,4 +52,116 @@ public class PropertiesConfig {
 
     @Value("${cache.redis.port}")
     private int redisPort;
+
+    public String getLogout() {
+        return logout;
+    }
+
+    public void setLogout(String logout) {
+        this.logout = logout;
+    }
+
+    public String getCheck() {
+        return check;
+    }
+
+    public void setCheck(String check) {
+        this.check = check;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getValidationQuery() {
+        return validationQuery;
+    }
+
+    public void setValidationQuery(String validationQuery) {
+        this.validationQuery = validationQuery;
+    }
+
+    public String getJdbcUsername() {
+        return jdbcUsername;
+    }
+
+    public void setJdbcUsername(String jdbcUsername) {
+        this.jdbcUsername = jdbcUsername;
+    }
+
+    public String getJdbcPassword() {
+        return jdbcPassword;
+    }
+
+    public void setJdbcPassword(String jdbcPassword) {
+        this.jdbcPassword = jdbcPassword;
+    }
+
+    public String getJdbcUrl() {
+        return jdbcUrl;
+    }
+
+    public void setJdbcUrl(String jdbcUrl) {
+        this.jdbcUrl = jdbcUrl;
+    }
+
+    public String getH2Url() {
+        return h2Url;
+    }
+
+    public void setH2Url(String h2Url) {
+        this.h2Url = h2Url;
+    }
+
+    public String getH2UserName() {
+        return h2UserName;
+    }
+
+    public void setH2UserName(String h2UserName) {
+        this.h2UserName = h2UserName;
+    }
+
+    public String getH2DatabaseName() {
+        return h2DatabaseName;
+    }
+
+    public void setH2DatabaseName(String h2DatabaseName) {
+        this.h2DatabaseName = h2DatabaseName;
+    }
+
+    public String getH2Quarz() {
+        return h2Quarz;
+    }
+
+    public void setH2Quarz(String h2Quarz) {
+        this.h2Quarz = h2Quarz;
+    }
+
+    public String getRedisHostName() {
+        return redisHostName;
+    }
+
+    public void setRedisHostName(String redisHostName) {
+        this.redisHostName = redisHostName;
+    }
+
+    public int getRedisPort() {
+        return redisPort;
+    }
+
+    public void setRedisPort(int redisPort) {
+        this.redisPort = redisPort;
+    }
 }

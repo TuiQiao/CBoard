51feb12e1f8ebb172f624aa0ab9ff3226f9e4b7c
guozhanxian <guozhanxian@gmail.com>
2019-09-10 19:45:55 +0800

    新建数据集后不跳页，再次保存，报名字重复错误。#682

--- src/main/java/org/cboard/services/DatasetService.java
+++ src/main/java/org/cboard/services/DatasetService.java
@@ -18,58 +18,64 @@ import java.util.Map;
 @Repository
 public class DatasetService {
 
-    @Autowired
-    private DatasetDao datasetDao;
+  @Autowired
+  private DatasetDao datasetDao;
 
-    public ServiceStatus save(String userId, String json) {
-        JSONObject jsonObject = JSONObject.parseObject(json);
-        DashboardDataset dataset = new DashboardDataset();
-        dataset.setUserId(userId);
-        dataset.setName(jsonObject.getString("name"));
-        dataset.setData(jsonObject.getString("data"));
-        dataset.setCategoryName(jsonObject.getString("categoryName"));
-        if (StringUtils.isEmpty(dataset.getCategoryName())) {
-            dataset.setCategoryName("默认分类");
-        }
-        Map<String, Object> paramMap = new HashMap<String, Object>();
-        paramMap.put("dataset_name", dataset.getName());
-        paramMap.put("user_id", dataset.getUserId());
-        paramMap.put("category_name", dataset.getCategoryName());
-        if (datasetDao.countExistDatasetName(paramMap) <= 0) {
-            datasetDao.save(dataset);
-            return new ServiceStatus(ServiceStatus.Status.Success, "success");
-        } else {
-            return new ServiceStatus(ServiceStatus.Status.Fail, "Duplicated name");
-        }
+  public ServiceStatus save(String userId, String json) {
+    JSONObject jsonObject = JSONObject.parseObject(json);
+    DashboardDataset dataset = new DashboardDataset();
+    dataset.setUserId(userId);
+    dataset.setName(jsonObject.getString("name"));
+    dataset.setData(jsonObject.getString("data"));
+    dataset.setCategoryName(jsonObject.getString("categoryName"));
+    if (StringUtils.isEmpty(dataset.getCategoryName())) {
+      dataset.setCategoryName("默认分类");
+    }
+    Map<String, Object> paramMap = new HashMap<String, Object>();
+    paramMap.put("dataset_name", dataset.getName());
+    paramMap.put("user_id", dataset.getUserId());
+    paramMap.put("category_name", dataset.getCategoryName());
+
+    if (jsonObject.getLong("id") != null) { // 判断前端是否传递id，如果有id，那么执行更新操作
+      return update(userId, json); // 如果判断数据库中有同名的数据集就执行更新操作
+    } else { // 否则执行插入操作
+      if (datasetDao.countExistDatasetName(paramMap) <= 0) {
+        datasetDao.save(dataset);
+        return new ServiceStatus(ServiceStatus.Status.Success, "success", dataset.getId()); // 把保存后生成的主键信息通过ServiceStatus传回给前台
+      } else {
+        return new ServiceStatus(ServiceStatus.Status.Fail, "Duplicated name");
+      }
     }
 
-    public ServiceStatus update(String userId, String json) {
-        JSONObject jsonObject = JSONObject.parseObject(json);
-        DashboardDataset dataset = new DashboardDataset();
-        dataset.setUserId(userId);
-        dataset.setId(jsonObject.getLong("id"));
-        dataset.setName(jsonObject.getString("name"));
-        dataset.setCategoryName(jsonObject.getString("categoryName"));
-        dataset.setData(jsonObject.getString("data"));
-        dataset.setUpdateTime(new Timestamp(Calendar.getInstance().getTimeInMillis()));
-        if (StringUtils.isEmpty(dataset.getCategoryName())) {
-            dataset.setCategoryName("默认分类");
-        }
-        Map<String, Object> paramMap = new HashMap<String, Object>();
-        paramMap.put("dataset_name", dataset.getName());
-        paramMap.put("user_id", dataset.getUserId());
-        paramMap.put("dataset_id", dataset.getId());
-        paramMap.put("category_name", dataset.getCategoryName());
-        if (datasetDao.countExistDatasetName(paramMap) <= 0) {
-            datasetDao.update(dataset);
-            return new ServiceStatus(ServiceStatus.Status.Success, "success");
-        } else {
-            return new ServiceStatus(ServiceStatus.Status.Fail, "Duplicated name");
-        }
-    }
+  }
 
-    public ServiceStatus delete(String userId, Long id) {
-        datasetDao.delete(id, userId);
-        return new ServiceStatus(ServiceStatus.Status.Success, "success");
+  public ServiceStatus update(String userId, String json) {
+    JSONObject jsonObject = JSONObject.parseObject(json);
+    DashboardDataset dataset = new DashboardDataset();
+    dataset.setUserId(userId);
+    dataset.setId(jsonObject.getLong("id"));
+    dataset.setName(jsonObject.getString("name"));
+    dataset.setCategoryName(jsonObject.getString("categoryName"));
+    dataset.setData(jsonObject.getString("data"));
+    dataset.setUpdateTime(new Timestamp(Calendar.getInstance().getTimeInMillis()));
+    if (StringUtils.isEmpty(dataset.getCategoryName())) {
+      dataset.setCategoryName("默认分类");
     }
+    Map<String, Object> paramMap = new HashMap<String, Object>();
+    paramMap.put("dataset_name", dataset.getName());
+    paramMap.put("user_id", dataset.getUserId());
+    paramMap.put("dataset_id", dataset.getId());
+    paramMap.put("category_name", dataset.getCategoryName());
+    if (datasetDao.countExistDatasetName(paramMap) <= 0) {
+      datasetDao.update(dataset);
+      return new ServiceStatus(ServiceStatus.Status.Success, "success");
+    } else {
+      return new ServiceStatus(ServiceStatus.Status.Fail, "Duplicated name");
+    }
+  }
+
+  public ServiceStatus delete(String userId, Long id) {
+    datasetDao.delete(id, userId);
+    return new ServiceStatus(ServiceStatus.Status.Success, "success");
+  }
 }
